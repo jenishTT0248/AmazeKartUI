fafdd601031b794e5300a0137f7c12b6aec85325
brijesh.sinojiya <brijesh.sinojiya@triveniglobalsoft.com>
2023-05-12 11:22:56 +0530

    Added suuplier api uri

--- src/constants/custom/urlConstant.ts
+++ src/constants/custom/urlConstant.ts
@@ -1,14 +1,18 @@
 const ROOT_URL = process.env.REACT_APP_API_URL;
 
 export const URL_CONSTANT = {
-    CategoryMaster: {
-        getAll: ROOT_URL + "/api/Category/GetAll",
-        GetById: ROOT_URL + "/api/Category/GetById",
-        DeleteData: ROOT_URL + "/api/Category/DeleteData",
-        Insert: ROOT_URL + "/api/Category/Insert",
-        Update: ROOT_URL + "/api/Category/Update",
-    }
-
-
-
-}
+  CategoryMaster: {
+    getAll: ROOT_URL + '/api/Category/GetAll',
+    GetById: ROOT_URL + '/api/Category/GetById',
+    DeleteData: ROOT_URL + '/api/Category/DeleteData',
+    Insert: ROOT_URL + '/api/Category/Insert',
+    Update: ROOT_URL + '/api/Category/Update',
+  },
+  Supplier: {
+    getAll: ROOT_URL + '/api/Supplier/GetAll',
+    GetById: ROOT_URL + '/api/Supplier/GetById',
+    DeleteData: ROOT_URL + '/api/Supplier/DeleteData',
+    Insert: ROOT_URL + '/api/Supplier/SaveData',
+    Update: ROOT_URL + '/api/Supplier/SaveData',
+  },
+};
